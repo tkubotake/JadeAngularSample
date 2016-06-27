1caa32b650607889e638b413d4fd0c95f0c0d148
kubota <kubota@ahacraft.jp>
2016-06-27 17:46:36 +0900

    add debug logs

--- app2.js
+++ app2.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongodb = require('mongodb');
+//var mongodb = require('mongodb');
 
 var app = express();
 var users;
@@ -13,22 +13,35 @@ app.use(express.static('front'));
 app.use(bodyParser.json());
 app.listen(3000);
 
+/*
 mongodb.MongoClient.connect("mongodb://localhost:27017/test", function(err, database) {
 	users = database.collection("users");
     });
+*/
 
 // 一覧取得
 app.get("/api/users", function(req, res) {
+	console.log("/api/users");
+	var items = [{_id:0, name: "勅使河原", age: 19},
+  {_id:1, name: "長宗我部", age: 20},
+  {_id:2, name: "小比類巻", age: 21}];
+	res.send(items);
+	/*
 	users.find().toArray(function(err, items) {
 		res.send(items);
 	    });
+	*/
     });
 
 // 個人取得
 app.get("/api/users/:_id", function(req, res) {
+	console.log("/api/users/:_id");
+	/*
 	users.findOne({_id: mongodb.ObjectID(req.params._id)}, function(err, item) {
 		res.send(item);
 	    });
+	*/
+	
     });
 
 // 追加・更新
@@ -42,7 +55,11 @@ app.post("/api/users", function(req, res) {
 
 // 削除
 app.delete("/api/users/:_id", function(req, res) {
+	console.log("delete");
+	res.send("delete");
+	/*
 	users.remove({_id: mongodb.ObjectID(req.params._id)}, function() {
 		res.send("delete");
 	    });
+		*/
     });
